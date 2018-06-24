938b21312d3bbc6f8503bd390ca5ecc6cf13545d
Kunal <kunallimaye@gmail.com>
2018-06-24 18:03:53 +1000

    Update auth0-variables.js

--- auth0-variables.js
+++ auth0-variables.js
@@ -1,4 +1,4 @@
 var AUTH0_CLIENT_ID='5EigxcHgzfeJxnlZhUdMplMBx29Qs7rV'; 
 var AUTH0_DOMAIN='finiteloop.au.auth0.com'; 
 //var AUTH0_CALLBACK_URL='http://localhost:3000/callback';
-var AUTH0_CALLBACK_URL='http://pizza42-test.127.0.0.1.nip.io/callback';
+var AUTH0_CALLBACK_URL='http://pizza42-test1.127.0.0.1.nip.io/callback';
