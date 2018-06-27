3b98183e2d289b30e8f884d980c85dc84369d077
Kunal Limaye <kunallimaye@gmail.com>
2018-06-27 21:51:17 +1000

    Yet to setup pizza menu

--- app/home/home.controller.js
+++ app/home/home.controller.js
@@ -13,6 +13,48 @@
     var vm = this;
     vm.auth = authService;
 
+    var pizzas = [
+      {
+        name: 'Pizza1',
+        description: 'Description1',
+        price: 1
+      },
+      {
+        name: 'Pizza2',
+        description: 'Description2',
+        price: 2
+      },
+      {
+        name: 'Pizza3',
+        description: 'Description3',
+        price: 3
+      },
+      {
+        name: 'Pizza4',
+        description: 'Description4',
+        price: 4
+      },
+      {
+        name: 'Pizza5',
+        description: 'Description5',
+        price: 5
+      },
+      {
+        name: 'Pizza6',
+        description: 'Description6',
+        price: 6
+      },
+      {
+        name: 'Pizza7',
+        description: 'Description7',
+        price: 7
+      },
+      {
+        name: 'Pizza8',
+        description: 'Description8',
+        price: 8
+      }
+    ];
   }
 
 })();
--- auth0-variables.js
+++ auth0-variables.js
@@ -5,5 +5,8 @@ var AUTH0_DOMAIN='finiteloop.au.auth0.com';
 // For local K8S setup (oc cluster up)
 //var AUTH0_CALLBACK_URL='http://pizza42-test1.127.0.0.1.nip.io/callback';
 
+// For proper K8S setup
+//var AUTH0_CALLBACK_URL='http://pizza42-test1.127.0.0.1.nip.io/callback';
+
 // For local npm start
 var AUTH0_CALLBACK_URL='http://0.0.0.0:8080/callback';
